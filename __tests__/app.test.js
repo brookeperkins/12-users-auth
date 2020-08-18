1833c79fe0285b946fb24bf0350650b1beefda55
brookeperkins <brookemmperkins@gmail.com>
2020-08-18 15:32:13 -0700

    updated data and tables

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -20,7 +20,7 @@ describe('app routes', () => {
     return client.end(done);
   });
 
-  test('returns animals', async() => {
+  test('returns todos', async() => {
 
     const expectation = [
       {
@@ -44,7 +44,7 @@ describe('app routes', () => {
     ];
 
     const data = await fakeRequest(app)
-      .get('/animals')
+      .get('/todos')
       .expect('Content-Type', /json/)
       .expect(200);
 
---
+++ data/todos.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    task: 'gargle rootbeer',
+    completed: false,
+    owner_id: 1
+  },
+  {
+    task: 'squats',
+    completed: false,
+    owner_id: 2
+  },
+  {
+    task: 'brush horse',
+    completed: false,
+    owner_id: 3
+  }
+];
