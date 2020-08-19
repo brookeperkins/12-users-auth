b0a66e09457daeec63182262c548dc97f1a13427
brookeperkins <brookemmperkins@gmail.com>
2020-08-18 22:08:31 -0700

    newToDo test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,48 +6,50 @@ const fakeRequest = require('supertest');
 const app = require('../lib/app');
 const client = require('../lib/client');
 
-describe('app routes', () => {
-  beforeAll(done => {
-    return client.connect(done);
-  });
+describe('routes', () => {
+  let token;
 
-  beforeEach(() => {
-    // TODO: ADD DROP SETUP DB SCRIPT
+  const newToDo = {
+    id: 4,
+    task: 'play scrabble',
+    completed: false,
+    user_id: 2
+  };
+
+  beforeAll(async done => {
     execSync('npm run setup-db');
+
+    client.connect();
+
+    const signInData = await fakeRequest(app)
+      .post('/auth/signup')
+      .send({
+        email: 'b@brooke.com',
+        password: '12345'
+      });
+
+    token = signInData.body.token;
+
+    return done();
+  
   });
 
   afterAll(done => {
     return client.end(done);
   });
 
-  test('returns todos', async() => {
-
-    const expectation = [
-      {
-        'id': 1,
-        'task': '',
-        'completed': 3,
-        'user_id': 1
-      },
-      {
-        'id': 2,
-        'task': '',
-        'completed': 4,
-        'user_id': 1
-      },
-      {
-        'id': 3,
-        'task': '',
-        'completed': 10,
-        'user_id': 1
-      }
-    ];
+  test('returns a new todo when creating new todo', async(done) => {
 
     const data = await fakeRequest(app)
-      .get('/todos')
+      .post('/api/todos')
+      .send(newToDo)
+      .set('Authorization', token)
       .expect('Content-Type', /json/)
       .expect(200);
 
-    expect(data.body).toEqual(expectation);
+    expect(data.body).toEqual(newToDo);
+
+    done();
   });
+  
 });
